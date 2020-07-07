b63daa4199af5a9fc767834932cfde4bb58cc6f3
Tawheed Haroon <tawheed.haroon@gmail.com>
2020-07-07 06:30:54 -0400

    help from tutor

---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "Cats Standing Still",
+            "Computer Sending Signals",
+            "Cascading Simple Sheets",
+            "Cascading Style Sheets"
+        ],
+        correctAnswer: "Cascading Style Sheets"
+    },
+    {
+        question: "What is a tag in HTML?",
+        options: [
+            "A function",
+            "Identifies the action of HTML",
+            "Gicves a name to the line of code",
+            "Defines how browser will format and display"
+        ],
+        correctAnswer: "Defines how browser will format and display"
+    },
+    {
+        question: "JavaScript commonly interacts with programming languages?",
+        options: [
+            "French and Russian",
+            "C++ and Java",
+            "jQuery and Java",
+            "HTML and CSS"
+        ],
+        correctAnswer: "HTML and CSS"
+    },
+    {
+        question: "What is the syntax for writing a function?",
+        options: [
+            "var = function",
+            "name function(code to execute){paramter1, parameter2}",
+            "function(parameter1, paramenter2) name",
+            "function name(parameter1, paramter2){code to execute}"
+        ],
+        correctAnswer: "function name(parameter1, paramter2){code to execute}"
+    },
+    {
+        question: "JavaScript has been described as...",
+        options: [
+            "light or dark roast.",
+            "giving design to HTML.",
+            "keeping the computer from crashing.",
+            "code that animates a website."
+        ],
+        correctAnswer: "code that animates a website."
+    }
+];
